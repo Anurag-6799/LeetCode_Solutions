1dd286f60b066d3167ebb4f6964ff8f174349f39
Anurag-6799 <singhanurag6799@gmail.com>
2025-12-19 14:18:15 +0530

    added more optimal solution of group anagram

--- group-anagrams.py
+++ group-anagrams.py
@@ -7,10 +7,20 @@
 # @lc code=start
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # tl = O(N.KlogK)
+        # mp = defaultdict(list)
+        # for s in strs:
+        #     sorted_key = "".join(sorted(s))
+        #     mp[sorted_key].append(s)
+        # return list(mp.values())
+
+        # tl - O(N.K)  
         mp = defaultdict(list)
         for s in strs:
-            sorted_key = "".join(sorted(s))
-            mp[sorted_key].append(s)
+            count = [0]*26
+            for ch in s:
+                count[ord(ch)-ord('a')]+=1
+            mp[tuple(count)].append(s)
         return list(mp.values())
 # @lc code=end
 
